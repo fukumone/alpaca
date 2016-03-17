7b8d69e15de14cf010eeb9b0688c5d18c783825c
t−fukui <fukuimone@gmail.com>
2016-03-17 13:28:19 +0900

    newフォーム作成途中

--- handler.go
+++ handler.go
@@ -6,9 +6,18 @@ import (
 	"github.com/t-fukui/alpaca/models"
 )
 
+type FormData struct {
+	Message models.Message
+}
+
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	Messages := []models.Message{}
 	db.Debug().Find(&Messages)
 	tpl := template.Must(template.ParseFiles("templates/index.html"))
 	tpl.Execute(w, &Messages)
+}
+
+func NewHandler(w http.ResponseWriter, r *http.Request){
+	tpl := template.Must(template.ParseFiles("templates/new.html"))
+	tpl.Execute(w, FormData{models.Message{}})
 }
